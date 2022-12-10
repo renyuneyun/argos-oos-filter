7afb429dcafde8ab06f92e9528dbf7e6251c8053
renyuneyun (Rui Zhao) <renyuneyun@gmail.com>
2022-12-10 19:41:35 +0000

    Initial version

---
+++ Argos_OOS_Filter.user.js
@@ -0,0 +1,59 @@
+// ==UserScript==
+// @name        Argos out-of-stock filter
+// @name:zh           Argos無貨過濾器
+// @name:zh-CN        Argos无货过滤器
+// @description       Filter products that are out-of-stock on Argos, by dimming them
+// @description:zh    過濾Argos上無貨的產品，將其變暗
+// @description:zh-CN    过滤Argos上无货的产品，将其变暗
+// @namespace   https://github.com/renyuneyun
+// @match       https://www.argos.co.uk/*
+// @grant       none
+// @version     0.1
+// @author      renyuneyun (Rui Zhao)
+// @description 2022/12/9 20:50:25
+// ==/UserScript==
+
+// TODO: optimization.
+// TODO: easier change of behaviour (remove vs dim)
+
+const MEASURE = false;
+
+async function checkProductTextAndRemove(elem) {
+  const promises = Array.from(elem.querySelectorAll("[data-test='component-product-card-availabilityLabel']")).map(e => {
+    (e.innerText == "Out of stock") &&
+      // elem.remove() // Use this to remove the element
+      elem.style.setProperty("opacity", 0.1);  // Or use this to dim the elemet
+  });
+  if (MEASURE)
+    await Promise.all(promises);
+}
+
+async function findAndRemove() {
+  const promises = Array.from(document.querySelectorAll("[itemtype='http://schema.org/Product']")).map(async elem => {
+    const res = checkProductTextAndRemove(elem);
+    if (MEASURE)
+      await res;
+    else
+      res;
+  });
+  if (MEASURE)
+    await Promise.all(promises);
+}
+
+async function main() {
+  if (MEASURE) {
+    const start = Date.now();
+    await findAndRemove();
+    const end = Date.now();
+    console.log(`Execution time: ${end - start} ms`);
+  } else {
+    findAndRemove();
+  }
+}
+
+(function() {
+  // Run the filter every 1.5 seconds, because Argos only dynamically loads the availability information.
+  // TODO: optimization.
+  setInterval(main, 1500);
+})();
+
---
+++ README.md
@@ -0,0 +1,6 @@
+Argos Out-Of-Stock Filter
+- - - - - -
+
+This is a simple Greasemonkey Script that filters out Out-Of-Stock items on Argos's results.
+
+By default, the out-of-stock items are dimmed by this script. You can change the code to delete the DOM nodes if you prefer.
